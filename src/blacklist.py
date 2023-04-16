79069e892bb99625bda5d58af85a6cba1c6f342a
Camilo <dc.perezc1@uniandes.edu.co>
2023-04-16 18:26:36 -0500

    health

--- src/blacklist.py
+++ src/blacklist.py
@@ -8,6 +8,10 @@ app.config.from_object("src.config.Config")
 
 db.init_app(app)
 
+@app.route("/blacklists/ping")
+def ping():
+    return "pong", 200
+
 api = Api(app)
 api.add_resource(VistaBlackList, '/blacklists')
 api.add_resource(VistaBlackUser, '/blacklists/<email>')
